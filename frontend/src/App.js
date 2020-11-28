5bc9b267a74894b2433a2b8b2289046743882e57
bhawna chopra <55947219+bhawnachopra2002@users.noreply.github.com>
2020-11-28 15:20:43 +0530

    Revert "front end retry"
    This reverts commit 720094987806f89db4ac6d37e3fc6a436059f967.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import {Route,BrowserRouter} from 'react-router-dom';
-import Join from './components/JoinComponent';
-import Chat from './components/ChatComponent';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
 import './App.css';
 
-const App=()=>{
-    return(
-      <BrowserRouter>
-      <Route path="/" exact component={Join}></Route>
-      <Route path="/chat" component={Chat}></Route>
-      </BrowserRouter>
-    )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
